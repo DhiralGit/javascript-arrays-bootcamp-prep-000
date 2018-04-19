a10c4dbd07ee2d37772d341c925a8b5ebd783c9f
Dhiral Patel <dhiralpatel794@gmail.com>
2018-04-19 17:13:39 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -15,3 +15,8 @@ function addElementToBeginningOfArray(array, element) {
   array = [...array,element];
   return array
 }
+
+function destructivelyAddElementToEndOfArray(array,element){
+  array.push(element);
+  return array
+}
