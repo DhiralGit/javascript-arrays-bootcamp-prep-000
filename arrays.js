b9b24aa5d8fd471ff3ae76248eb441c11f868cd9
Dhiral Patel <dhiralpatel794@gmail.com>
2018-04-19 17:41:40 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -1,1 +1,21 @@
 var chocolateBars = ['snickers', 'hundred grand', 'kitkat', 'skittles']
+
+function addElementToBeginningOfArray(array, element) {
+  array = [element, ...array];
+  return array;
+}
+
+function destructivelyAddElementToBeginningOfArray(array, element) {
+  array.unshift(element);
+  return array
+}
+
+function addElementToBeginningOfArray(array, element) {
+  array = [...array,element];
+  return array
+}
+
+function destructivelyAddElementToEndOfArray(array,element){
+  array.push(element);
+  return array
+}
