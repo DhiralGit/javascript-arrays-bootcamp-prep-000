bacc44250f72efa585bad4bbdb6a4942be95a150
Dhiral Patel <dhiralpatel794@gmail.com>
2018-04-19 18:50:50 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -31,6 +31,6 @@ function accessElementInArray(array, index) {
 }
 
 function destructivelyRemoveElementFromBeginningOfArray(array) {
-  array.unshift(element)
+  array.unshift()
   return array;
 }
