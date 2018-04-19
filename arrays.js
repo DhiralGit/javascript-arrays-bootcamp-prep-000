74feea4493ada2a4aa84fced4dc3eca275a53ee3
Dhiral Patel <dhiralpatel794@gmail.com>
2018-04-19 17:55:30 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -19,3 +19,8 @@ function destructivelyAddElementToEndOfArray(array, element) {
   array.push(element)
   return array;
 }
+
+function destructivelyAddElementToEndOfArray(array, element) {
+  array.push(element)
+  return array;
+}
