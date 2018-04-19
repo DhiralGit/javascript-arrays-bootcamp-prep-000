0f38cbc8e70130217bfafce2756226cbc056d950
Dhiral Patel <dhiralpatel794@gmail.com>
2018-04-19 17:39:32 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -2,7 +2,7 @@ var chocolateBars = ['snickers', 'hundred grand', 'kitkat', 'skittles']
 chocolateBars.unshift('gums')
 
 function addElementToBeginningOfArray(array, element) {
-  return [element, ...array];
+  array = [element, ...array];
   return array;
 }
 
