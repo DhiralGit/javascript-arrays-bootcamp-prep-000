6be7d370821db2a4cb1cc205d1d05b5773a4f346
Dhiral Patel <dhiralpatel794@gmail.com>
2018-04-19 17:27:43 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -1,49 +1,22 @@
-var ingredient1 ="bread"
-var ingredient2 = "mild cheese"
-var ingredient3 = "sharp cheese"
-var ingredient4 = "butter"
-var ingredient5 = "tomato"
-var ingredient6 = "garlic"
-var gailledCheeseIngredients=[
-  'bread',
-  'mild cheese',
-  'sharp cheese',
-  'butter',
-  'tomato',
-  'garlic'
-  ]
-  var tomatoSauceIngredients = [
-    'tomato',
-    'garlic',
-    'olive oil',
-    'basil',
-    'oregano'
-    ]
-    var chocolateBars = [
-      'snicker',
-      'hundred grand',
-      'kitkat',
-      'skittles']
-      
-      function addElementToBeginningOfArray(array, element){
-         array = [element,...array];
-        return array;
-      }
-      function destructivelyAddElementToBeginningOfArray(array,element)
-      {
-        var newArray = [];
-        newArray=array;
-         newArray.unshift(element)
-        return newArray;
-        
-      }
-      function addElementToEndOfArray(array,element){
-        
-        array = [...array,element];
-        return array;
-      }
-      function destructivelyAddElementToEndOfArray(array,element){
-        var newArray = array;
-        array.push(element);
-        return newArray
-      }
+var chocolateBars = ['snickers', 'hundred grand', 'kitkat', 'skittles']
+chocolateBars.unshift('gums')
+
+function addElementToBeginningOfArray(array, element) {
+  return [element, ...array];
+  return array;
+}
+
+function destructivelyAddElementToBeginningOfArray(array, element) {
+  array.unshift(element);
+  return array
+}
+
+function addElementToBeginningOfArray(array, element) {
+  array = [...array,element];
+  return array
+}
+
+function destructivelyAddElementToEndOfArray(array,element){
+  array.push(element);
+  return array
+}
