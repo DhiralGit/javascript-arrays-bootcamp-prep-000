7dc7bbfcc7f1866cae351caef5fa33d61d58eac6
Dhiral Patel <dhiralpatel794@gmail.com>
2018-04-19 15:55:31 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -11,5 +11,5 @@ function destructivelyAddElementToBeginningOfArray(array, element) {
 }
 
 function addElementToBeginningOfArray(array, element) {
-  return [...element, array];
+  return [element, array];
 }
